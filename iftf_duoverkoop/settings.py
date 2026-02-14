346629b2582a1903fd3dfae934f339f817df5e7d
Maarten Magits <mmaartendisc@gmail.com>
2026-02-14 18:59:37 +0100

    database

--- iftf_duoverkoop/settings.py
+++ iftf_duoverkoop/settings.py
@@ -12,9 +12,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
-DEBUG = os.environ.get("DEBUG") == "True"
+SECRET_KEY = os.environ.get("SECRET_KEY") or "django-insecure-CHANGE_ME"
+DEBUG = (os.environ.get("DEBUG") == "True") or True
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,11 +75,9 @@ WSGI_APPLICATION = 'iftf_duoverkoop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+DATABASE_URL = os.environ.get('DATABASE_URL')
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=f'sqlite:///{BASE_DIR}/db.sqlite3', conn_max_age=600)
 }
 
 
